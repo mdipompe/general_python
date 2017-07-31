d70d7562eecf1b9f47348c702ca33a5a9167d23e
Mike DiPompeo <mdipompe@gmail.com>
2017-07-31 16:31:04 -0400

    Utility for transforming between various coordinate systems

---
+++ coord_trans.py
@@ -0,0 +1,80 @@
+'''
+Basic transformations between cartesian, polar, and spherical coordinates.
+If degree is True, inputs are assumed to be in degrees and outputs will be as well
+'''
+import numpy as np
+
+def cart2pol(x, y, degree=False):
+    theta = np.arctan2(y, x)
+    rho = np.hypot(x, y)
+    if degree:
+        theta = theta * (180./np.pi)
+    return theta, rho
+
+def pol2cart(theta, rho, degree=False):
+    if degree:
+        theta = theta * (np.pi/180.)
+    x = rho * np.cos(theta)
+    y = rho * np.sin(theta)
+    return x, y
+
+def cart2sph(x, y, z, degree=False):
+    hxy = np.hypot(x, y)
+    r = np.hypot(hxy, z)
+    el = np.arctan2(z, hxy)
+    az = np.arctan2(y, x)
+    if degree:
+        el = el * (180./np.pi)
+        az = az * (180./np.pi)
+    return az, el, r
+
+def sph2cart(az, el, r, degree=False):
+    if degree:
+        az = az * (np.pi/180.)
+        el = el * (np.pi/180.)
+    rcos_theta = r * np.cos(el)
+    x = rcos_theta * np.cos(az)
+    y = rcos_theta * np.sin(az)
+    z = r * np.sin(el)
+    return x, y, z
+
+def cart2cyl(x, y, z, degree=False):
+    rho = np.hypot(x, y)
+    if x == 0 & y == 0:
+        phi = 0
+    if x == 0 & y != 0:
+        phi = np.arcsin2(y,rho)
+    if x > 0:
+        phi = np.arctan2(y,x)
+    if x < 0:
+        phi = (-1.) * arcsin2(y,rho) + np.pi
+    if degree:
+        phi = phi * (180./np.pi)
+    return rho, phi, z
+
+def cyl2cart(rho, phi, z, degree=False):
+    if degree:
+        phi = phi * (np.pi/180.)
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return x, y, z
+
+def cyl2sph(rho, phi, z, degree=False):
+    if degree:
+        phi = phi * (np.pi/180.)
+    r = np.hypot(rho,z)
+    theta = np.arctan2(z,rho)
+    if degree:
+        theta = theta * (180./np.pi)
+    return r, theta, phi
+
+def sph2cyl(r, theta, phi, degree=False):
+    if degree:
+        theta = theta * (np.pi/180.)
+        phi = phi * (np.pi/180.)
+    rho = r * np.cos(theta)
+    z = r * np.sin(theta)
+    return rho, phi, z
+        
+    
+    
