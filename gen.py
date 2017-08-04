8e703a3d39e62807486d3dc9c04938d7e2dd848b
Mike DiPompeo <mdipompe@gmail.com>
2017-08-04 18:14:43 -0400

    Merged basics in gen.py

--- gen.py
+++ gen.py
@@ -1,11 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+
 '''
 Define bins uniformly spaced in log space. Uses numpys logspace, but allows
 user to specify number of points per dex, and returns bin edges as well as 
 centers
 '''
-import numpy as np
-import matplotlib.pyplot as plt
-
 def log_bins(low=0.001, high=2.1, nperdex=5):
     num = ((np.log10(high) - np.log10(low)) * nperdex) + 1
     bin_edges = np.logspace(np.log10(low), np.log10(high), num=num)
@@ -19,3 +20,26 @@ def log_bins(low=0.001, high=2.1, nperdex=5):
 
     return bin_edges, bin_cents
 
+'''
+Generate random indices with no repeats for random sub-sampling
+'''
+def rand_indices(inlen,outlen,seed=None):
+    if outlen > inlen:
+        print('rand_indices error: '
+              'N_out > N_in, cannot generate non-repeating random indices')
+        return
+    if seed:
+        np.random.seed(seed)
+    indx = np.arange(0,inlen)
+    np.random.shuffle(indx)
+    indx = indx[0:outlen]
+    return indx
+
+'''
+Read in fits file and get just the data portion
+'''
+def readfits(infile,ext=1):
+    data=fits.open(infile)[ext]
+    return data.data
+
+    
--- rands.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def rand_indices(inlen,outlen,seed=None):
-    if seed:
-        np.random.seed(seed)
-    indx = np.arange(0,inlen)
-    np.random.shuffle(indx)
-    indx = indx[0:outlen]
-    return indx
